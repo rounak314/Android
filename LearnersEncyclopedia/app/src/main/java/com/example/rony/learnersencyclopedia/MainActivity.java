3a1e069a34bc18f0831dcf87a6303622730af449
rdx314 <rounak2003@gmail.com>
2019-01-04 19:33:54 +0530

    Android app for blogger page

---
+++ LearnersEncyclopedia/app/src/main/java/com/example/rony/learnersencyclopedia/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.rony.learnersencyclopedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://educatingworldthebestwork.blogspot.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
